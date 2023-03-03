2381faddad9b56a555978e2c45827a2ce14b795f
staytheknight <staytheknight@gmail.com>
2023-03-03 11:04:36 -0700

    Added todo comments for next steps

--- 589-689-skeleton/SandCell.cpp
+++ 589-689-skeleton/SandCell.cpp
@@ -6,11 +6,14 @@ void test() {
 	_adhesion = 10.f;
 	_height = 10.f;
 
-
+	// The idea is that each data type we want to track for each cell is pushed to a
+	// vector array. These vector arrays are index aligned, so the data should be
+	// tied to a particular point (might be better as a struct)
 	for (int j = 0; j < _area_length_z; j++ {
 		for (int i = 0; i < _area_length_x; i++) {
 			heights.push_back(_height);
 			adhesions.push_back(_adhesion);
+			// TODO: push back vertices, and change the y value based on the height value passed
 		}
 	}
 
