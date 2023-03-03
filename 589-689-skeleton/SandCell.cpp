17973695561d4a3305bd828099c103d7387225c1
staytheknight <staytheknight@gmail.com>
2023-03-03 10:59:30 -0700

    Basic sand cell struct idea
    not tested and just a rough idea

--- 589-689-skeleton/SandCell.cpp
+++ 589-689-skeleton/SandCell.cpp
@@ -1,2 +1,18 @@
-#include "DataStruct.h"
+#include "SandCell.h"
+
+
+void test() {
+	// Test value for adhesion
+	_adhesion = 10.f;
+	_height = 10.f;
+
+
+	for (int j = 0; j < _area_length_z; j++ {
+		for (int i = 0; i < _area_length_x; i++) {
+			heights.push_back(_height);
+			adhesions.push_back(_adhesion);
+		}
+	}
+
+}
 
--- 589-689-skeleton/SandCell.h
+++ 589-689-skeleton/SandCell.h
@@ -1,5 +1,17 @@
-struct sandCell {
-	int ID;
-	float height;
-	float adhesion;
-};
+#include <vector>
+#include "Geometry.h"
+
+
+// Size of the simulation, it will be a square of
+// sim_size x sim_size
+int _area_length_x;
+int _area_length_z;
+
+float _height;
+float _adhesion;
+
+std::vector<float> heights;
+std::vector<float> adhesions;
+std::vector<CPU_Geometry> locations;
+
+void test();
