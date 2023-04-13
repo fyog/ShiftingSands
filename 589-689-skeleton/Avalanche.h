e988c1df216c39cdaaedc2aeb0eaa8513fbf04bd
fyog <eric.alex.ross@gmail.com>
2023-04-12 20:15:10 -0600

    ...

--- 589-689-skeleton/Avalanche.h
+++ 589-689-skeleton/Avalanche.h
@@ -16,10 +16,10 @@ bool check_repose_right(CPU_Geometry& surface, std::vector<float> heights,  int 
 
 bool check_repose_up(CPU_Geometry& surface, std::vector<float> heights, int x, int y);
 
-void avalanche_up(CPU_Geometry& surface, std::vector<float> heights,  int x, int y);
+void avalanche_up(CPU_Geometry& surface, std::vector<float> &heights,  int x, int y);
 
-void avalanche_left(CPU_Geometry& surface, std::vector<float> heights,  int x, int y);
+void avalanche_left(CPU_Geometry& surface, std::vector<float> &heights,  int x, int y);
 
-void avalanche_down(CPU_Geometry& surface, std::vector<float> heights,  int x, int y);
+void avalanche_down(CPU_Geometry& surface, std::vector<float> &heights,  int x, int y);
 
-void avalanche_right(CPU_Geometry& surface, std::vector<float> heights,  int x, int y);
+void avalanche_right(CPU_Geometry& surface, std::vector<float> &heights,  int x, int y);
